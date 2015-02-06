b0db33f660331c6cba0e2b399f70d1385f9ed484
Tyler Hargett <tyler.t.hargett@gmail.com>
2015-02-05 19:35:43 -0600

    added search. still needs work

---
+++ StockApp/iOS_app/StockApp/StockApp/SearchTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewCell.h
+//  StockApp
+//
+//  Created by Tyler Hargett on 2/5/15.
+//  Copyright (c) 2015 teamE1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewCell : UITableViewCell
+
+@end
