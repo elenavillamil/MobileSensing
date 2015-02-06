e4818456b56d278dfdc0e424797e0d242cd38038
Tyler Hargett <tyler.t.hargett@gmail.com>
2015-02-06 02:43:10 -0600

    something is here

---
+++ StockApp/iOS_app/StockApp/StockApp/OwnedStock.h
@@ -0,0 +1,15 @@
+//
+//  OwnedStock.h
+//  StockApp
+//
+//  Created by Tyler Hargett on 2/6/15.
+//  Copyright (c) 2015 teamE1. All rights reserved.
+//
+
+#import "Stock.h"
+
+@interface OwnedStock : Stock {
+    NSInteger amount;
+}
+
+@end
---
+++ StockApp/iOS_app/StockApp/StockApp/OwnedStock.m
@@ -0,0 +1,13 @@
+//
+//  OwnedStock.m
+//  StockApp
+//
+//  Created by Tyler Hargett on 2/6/15.
+//  Copyright (c) 2015 teamE1. All rights reserved.
+//
+
+#import "OwnedStock.h"
+
+@implementation OwnedStock
+
+@end
