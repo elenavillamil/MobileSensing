04db8fa10a19a30f2da8b8eecdc9168c5e534a8c
elenavillamil <mvillamilrod@smu.edu>
2015-02-22 16:51:35 -0600

    Step Count auto layout and navigation bar to settings

---
+++ Assigment 3/Step Counter/Step Counter/SettingsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsViewController.swift
+//  Step Counter
+//
+//  Created by Elena Villamil on 2/22/15.
+//  Copyright (c) 2015 Elena Villamil. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
