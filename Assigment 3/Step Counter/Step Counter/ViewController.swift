f3d8bd2048fd39f4f5ae8d12b18bbf69ae320a57
Candie Solis <home@candiesolis.com>
2015-02-25 17:56:25 -0600

    modified ViewController to check for use of pedometer and activity

--- Assigment 3/Step Counter/Step Counter/ViewController.swift
+++ Assigment 3/Step Counter/Step Counter/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import CoreMotion
+
 
 class ViewController: UIViewController {
 
+    
+    let activityManager = CMMotionActivityManager()
+    let customQueue = NSOperationQueue()
+    let pedometer = CMPedometer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if CMMotionActivityManager.isActivityAvailable(){
+            self.activityManager.startActivityUpdatesToQueue( self.customQueue)
+                { (activity:CMMotionActivity!) -> Void in
+                    
+                    dispatch_async(dispatch_get_main_queue())
+                        {
+                            self.textLabel.text = activity.description
+                    }
+            }
+        }
+        
+        
+        if CMPedometer.isStepCountingAvailable(){
+            self.pedometer.startPedometerUpdatesFromDate(NSDate()) { (pedData: CMPedometerData!, error: NSError!) -> Void in
+                
+                dispatch_async(dispatch_get_main_queue())
+                    {
+                        self.textLabelLower.text = pedData.description
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
