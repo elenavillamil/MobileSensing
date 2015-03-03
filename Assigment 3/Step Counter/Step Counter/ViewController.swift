e54b20a86b5773bc48aee1c3ea04a1b39364d876
elenavillamil <mvillamilrod@smu.edu>
2015-03-02 21:54:17 -0600

    added pedometer and cmmonitor code and adjusted storyboard

--- Assigment 3/Step Counter/Step Counter/ViewController.swift
+++ Assigment 3/Step Counter/Step Counter/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
     let activityManager = CMMotionActivityManager()
     let customQueue = NSOperationQueue()
     let pedometer = CMPedometer()
-    
+    let twentyFourHours = 24 * 3600 as NSTimeInterval
+
     @IBOutlet weak var yesterdayStepsLabel: UILabel!
     @IBOutlet weak var todayStepsLabel: UILabel!
     @IBOutlet weak var stepsToGoalLabel: UILabel!
@@ -25,30 +26,71 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if CMMotionActivityManager.isActivityAvailable(){
-            self.activityManager.startActivityUpdatesToQueue( self.customQueue)
-                { (activity:CMMotionActivity!) -> Void in
-                    
-                    dispatch_async(dispatch_get_main_queue())
-                        {
-                            self.currentActivityLabel.text = activity.description
-                    }
-            }
-        }
+        fetchMotionActivityData()
         
-        
+        fetchPedometerData()
+    }
+    
+    func fetchPedometerData() {
         if CMPedometer.isStepCountingAvailable(){
-            self.pedometer.startPedometerUpdatesFromDate(NSDate()) { (pedData: CMPedometerData!, error: NSError!) -> Void in
-                
-                dispatch_async(dispatch_get_main_queue())
-                    {
-                        self.todayStepsLabel.text = pedData.description
+            self.pedometer.startPedometerUpdatesFromDate(NSDate()) {
+                (pedData: CMPedometerData!, error: NSError!) -> Void in dispatch_async(dispatch_get_main_queue()) {
+                    self.todayStepsLabel.text = NSString(format: "%d", pedData.numberOfSteps)
+                    
+                    // add accessor to steps in goal in settings
+                    // self.stepsToGoLabel.text = *goalSteps - pedData.numberOfSteps
+                }
+            }
+            
+            //MAY BE ILLEGAL -- ASK ELENA
+            self.pedometer.startPedometerUpdatesFromDate(NSDate(timeIntervalSinceNow: -twentyFourHours)) {
+                (pedData: CMPedometerData!, error: NSError!) -> Void in dispatch_async(dispatch_get_main_queue()) {
+                    self.todayStepsLabel.text = NSString(format: "%d", pedData.numberOfSteps)
                 }
             }
         }
-        
     }
 
+    
+    func fetchMotionActivityData()
+    {
+        if CMMotionActivityManager.isActivityAvailable()
+        {
+        
+            self.activityManager.startActivityUpdatesToQueue(self.customQueue){
+                (activity:CMMotionActivity!) -> Void in dispatch_async(dispatch_get_main_queue()) {
+                    self.currentActivityLabel.text = activity.description
+                }
+            }
+            
+            // Prepare activity updates
+            activityManager.startActivityUpdatesToQueue(self.customQueue) {
+                data in
+                dispatch_async(dispatch_get_main_queue()) {
+                    if data.running {
+                        //self.activityImageView.image = UIImage(named: "run")
+                        self.currentActivityLabel.text = "Running"
+                    } else if data.cycling {
+                        //self.activityImageView.image = UIImage(named: "cycle")
+                        self.currentActivityLabel.text = "Cycling"
+                    } else if data.walking {
+                        //self.activityImageView.image = UIImage(named: "walk")
+                        self.currentActivityLabel.text = "Walking"
+                    } else if data.walking {
+                        //self.activityImageView.image = UIImage(named: "nil")
+                        self.currentActivityLabel.text = "Still"
+                    } else if data.walking {
+                        //self.activityImageView.image = UIImage(named: "nil")
+                        self.currentActivityLabel.text = "Driving"
+                    } else {
+                        //self.activityImageView.image = nil
+                        self.currentActivityLabel.text = "Stationary"
+                    }
+                }
+            }
+            
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
