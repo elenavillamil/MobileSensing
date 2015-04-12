87773d5bd808b584912b9988fcbf4a288cdb45f9
Tyler Hargett <tyler.t.hargett@gmail.com>
2015-04-12 06:42:38 -0500

    added base iOS project to repo

---
+++ Assignment 6/Drone Regonizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drone Regonizer
+//
+//  Created by Tyler Hargett on 4/12/15.
+//  Copyright (c) 2015 Tyler Hargett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
